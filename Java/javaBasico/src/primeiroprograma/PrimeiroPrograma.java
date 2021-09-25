10a5be9c12b972dadbb7a23414a4a537c4400a81
luanhoffmann <61809243+luanlipperthoffmann@users.noreply.github.com>
2021-09-25 19:23:41 -0300

    Início do curso basico de Java do Curso em Video do professor gustavo Guanabara

---
+++ Java/javaBasico/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package primeiroprograma; //pacote primeiro programa
+
+public class PrimeiroPrograma { //classe principal
+
+    public static void main(String[] args) { //metodo principal dentro da classe
+        //Existem diferenças entre letras maisculas e minusculas
+        //O pacote é escrito com letras minusculas
+        //A classe é escrita tudo junto com a primeira letra maiscula
+        System.out.println("Olá Mundo!");
+    }
+}
